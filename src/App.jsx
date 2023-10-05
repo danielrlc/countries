9c6956f5027772cd74cb2b581807537010b48922
Daniel Clarke <8128944+danielrlc@users.noreply.github.com>
2023-10-05 04:40:15 +0300

    Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,16 @@ function App() {
       <ul className="grid sm:grid-cols-2 lg:grid-cols-3">
         {countries.map((country) => (
           <li key={country.cca2} className="bg-white m-8">
-            <img className="w-full" src={country.flags.png} alt={`${country.name.common} flag`} />
+            <img
+              className="w-full"
+              src={country.flags.png}
+              alt={`${country.name.common} flag`}
+            />
             <div className="p-6">
-
-            <div className="text-lg font-bold">{country.name.common}</div>
-            <div>Population: {country.population}</div>
-            <div>Region: {country.region}</div>
-            <div>Capital: {country.capital}</div>
+              <div className="text-lg font-bold">{country.name.common}</div>
+              <div>Population: {country.population}</div>
+              <div>Region: {country.region}</div>
+              <div>Capital: {country.capital}</div>
             </div>
           </li>
         ))}
