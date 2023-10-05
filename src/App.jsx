371b2d0c6780cdad72f1279d872287d04438cc02
Daniel Clarke <8128944+danielrlc@users.noreply.github.com>
2023-10-05 04:38:42 +0300

    Countries shown in a grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { useState } from "react";
 import countriesData from "../data/countries.json";
 
 function App() {
-  const [countries, setCountries] = useState(countriesData);
-  console.log(countries);
+  const countries = countriesData;
 
   return (
-    <ul>
-      {countries.map((country) => (
-        <li>{country.name.common}</li>
-      ))}
-    </ul>
+    <main className="bg-gray-100">
+      <ul className="grid sm:grid-cols-2 lg:grid-cols-3">
+        {countries.map((country) => (
+          <li key={country.cca2} className="bg-white m-8">
+            <img className="w-full" src={country.flags.png} alt={`${country.name.common} flag`} />
+            <div className="p-6">
+
+            <div className="text-lg font-bold">{country.name.common}</div>
+            <div>Population: {country.population}</div>
+            <div>Region: {country.region}</div>
+            <div>Capital: {country.capital}</div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </main>
   );
 }
 
