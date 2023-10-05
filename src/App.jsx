74a2d5d17d12cf89a7f148fec82da267d5d1fb34
Daniel Clarke <8128944+danielrlc@users.noreply.github.com>
2023-10-05 05:08:41 +0300

    Tidy up grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ function App() {
   const countries = countriesData;
 
   return (
-    <main className="bg-gray-100">
-      <ul className="grid sm:grid-cols-2 lg:grid-cols-3">
+    <main className="bg-gray-100 flex justify-center p-4 lg:p-16">
+      <ul className="grid gap-[70px] sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {countries.map((country) => (
-          <li key={country.cca2} className="bg-white m-8">
+          <li key={country.cca2} className="bg-white w-[250px]">
             <img
-              className="w-full"
+              className="w-full h-[150px]"
               src={country.flags.png}
               alt={`${country.name.common} flag`}
             />
